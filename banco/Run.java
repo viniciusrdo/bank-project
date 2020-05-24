5fabb3a1e7c52fcba83bc10c11af3b6a8ae20783
Vinicius <vinioliveira015@gmail.com>
2020-05-24 20:22:35 -0300

    I put some new tests in the code, these are related to the access modifiers

--- banco/Run.java
+++ banco/Run.java
@@ -5,26 +5,18 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int valor = 250;
-		
-		boolean testeSaca;
-		
 		Conta contaNova = new Conta();
 		
-		contaNova.agencia = "6554";
-		
-		contaNova.saldo = 1000;
-		
-		testeSaca = contaNova.saca(valor);
-		
-		if(testeSaca)
-			System.out.println("O valor de " + valor + " foi sacado com sucesso!");
-		else	
-			System.out.println("Saldo Insulficiente!");
 		
 		
-		System.out.println(contaNova.saldo);
-			
+		/*
+		 * testeSaca = contaNova.saca(valor);
+		 * 
+		 * if(testeSaca) System.out.println("O valor de " + valor +
+		 * " foi sacado com sucesso!"); else System.out.println("Saldo Insulficiente!");
+		 */
+		
+		
 	
 
 	}
