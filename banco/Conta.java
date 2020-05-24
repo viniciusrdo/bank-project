8b184dd22f263716020ed3e15de22182d5396d33
Vinicius <vinioliveira015@gmail.com>
2020-05-24 20:20:11 -0300

    I've finished tho set the access modifiers, and i've added a constructor example at the script.

--- banco/Conta.java
+++ banco/Conta.java
@@ -8,15 +8,24 @@ public class Conta {
 	
 	private String agencia;
 	
-	
 	private double saldo;
 			
-	
 	private String data_Abertura;
+
 	
 	
+	// Construtores
 	
-	boolean saca(double valor){
+	Conta(){}
+	
+	
+	Conta(String titular){
+		this.titular = titular;
+	}
+	
+	// Métodos
+	
+	boolean saca(double valor){		// Saca
 		if(valor <= saldo) {
 			double novoSaldo = this.saldo - valor;
 			this.saldo = novoSaldo;
@@ -25,12 +34,56 @@ public class Conta {
 		else return false;
 	}
 	
-	void deposita(double quantidade){
+	void deposita(double quantidade){		// Deposita
 		this.saldo	+=	quantidade;
 	}	
 	
-	void calculaRendimento(){
+	void calculaRendimento(){		//Calcula Rendimento
 		this.saldo = this.saldo * 1.1;
 	}
+	
+	// Getters and Setters
+	
+	public int getNumero(){		
+		return numero;
+	}
+	
+	public void setNumero(int numero){
+		this.numero = numero;
+	}
+	
+	
+	public String getTitular(){
+		return titular;
+	}
+	
+	public void setTitular(String titular){
+		this.titular = titular;
+	}
+	
+	public String getAgencia(){
+		return agencia;
+	}
+	
+	public void setAgencia(String Agencia){
+		this.agencia = Agencia;
+	}
+	
+	public String get_Data_Abertura(){
+		return data_Abertura;
+	}
+	
+	public void set_Data_Abertura(String data_Abertura){
+		this.data_Abertura = data_Abertura;
+	}
+	
+	// Método que imprime o comprovante
+	
+	public void printa_Dados(){
+		System.out.println("\n\nNome do titular: " + titular + "\nNumero da conta: " + numero + 
+		"\nNumero da Agencia: " + agencia + "\nData de Abertura: " + data_Abertura + "\n Saldo = " + saldo);
+	}
+	
+	
 	
 }	
